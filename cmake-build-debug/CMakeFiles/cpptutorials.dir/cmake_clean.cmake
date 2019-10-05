205cdace6241890324fef9cdf8c5dafdd681923c
neeladalja14 <nadal001@plattsburgh.edu>
2019-10-05 02:28:55 -0700

    this is new start

---
+++ cmake-build-debug/CMakeFiles/cpptutorials.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpptutorials.dir/main.cpp.obj"
+  "cpptutorials.pdb"
+  "cpptutorials.exe"
+  "cpptutorials.exe.manifest"
+  "libcpptutorials.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpptutorials.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cpptutorials.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpptutorials.
+# This may be replaced when dependencies are built.
