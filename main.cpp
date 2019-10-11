1b5ae639066eb5f6c6da5a42ab23c78ff44b2fd5
neeladalja14 <nadal001@plattsburgh.edu>
2019-10-11 15:41:25 -0700

    this is multi dimentional array example for multiplying the table with 2 having different values at i and j elements of an array.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main(){
     // multidimentional arraay is created for animals. it has structure like 2x3
-    string animal[2][3]= {{"dog",   "cat",     "lion"},
-                          {"mouse", "girrafe", "donkey"}
+    int animal[2][3]= {{2,3,4},
+                          {6,7,8}
     };
 
     //the first loop is actually going through no of rows. if we change the values, the difference can be seen.
     for(int i=0; i<2; i++){
         //the second loop goes through the elements inside the two arraays. if we hange thee value to 2, then it will rint two elements of both row 1 and 2.
         for(int j=0; j<3;j++){
+            animal[i][j] *= 2;
             cout << animal[i][j] <<" "<< flush;
         }
         cout<<endl;
