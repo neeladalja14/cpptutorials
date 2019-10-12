692b402138dcec4590cfeebf9213e7cd704570f2
neeladalja14 <nadal001@plattsburgh.edu>
2019-10-12 00:22:00 -0700

    this is simple project for the classes and working with class/header and many files at once.

--- cat.cpp
+++ cat.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 //this void function is connected to cat class as blueprint. this function stays under cat class only(::) denotes tha
 void cat::speak(){
-    cout << "meaow"<<endl;
+    cout << "meow"<<endl;
 }
 
 //this is the same as above function.
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "cat.h"
 
 int main() {
-    //the cat classs is called and the variable given is cat1.
+    //the cat class is called and the variable given is cat1.
     cat cat1;
     //thee cat1 variable calls the speak and jump functions.
     //therefore to call the function from a class we write (cat1.speak();, cat1.jump();).
